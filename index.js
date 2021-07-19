2e1c0864e7d9bbf6001e582ef8c0395b992c9f1c
Christopher Cohen <chris@cscohen.com>
2021-07-19 17:07:27 -0600

    Completed assignment

--- index.js
+++ index.js
@@ -1,11 +1,27 @@
-function scuberGreetingForFeet(){
+function scuberGreetingForFeet(numberFeet){
   // Write your code here!
+  if (numberFeet <= 400) {
+  return 'This one is on me!'
+  } else if (numberFeet >= 2500) {
+    return 'No can do.'
+} else 
+   return 'I will gladly take your thirty bucks.'
 }
 
-function ternaryCheckCity(){
-  // Write your code here!
+function ternaryCheckCity(cityName){
+  return (cityName === 'NYC' ? 'Ok, sounds good.' : 'No go.')
 }
-
-function switchOnCharmFromTip(){
+function switchOnCharmFromTip(theTip){
   // Write your code here!
+  let generous = 10
+  switch (true) {
+    case theTip === 'generous':
+      return 'Thank you so much.'
+      break;    
+    case theTip === 'not as generous':
+      return 'Thank you.'
+      break;
+    default:
+      return 'Bye.'
+  }
 }
